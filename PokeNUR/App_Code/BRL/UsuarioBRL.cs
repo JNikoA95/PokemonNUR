cc2a4cee3fbb7f6abe0cc2cacdac1825c321face
JNikoA95 <andradenik09@gmail.com>
2017-09-12 13:46:02 -0400

    Modulo de registro de usuarios primera parte

---
+++ PokeNUR/App_Code/BRL/UsuarioBRL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de UsuarioBRL
+/// </summary>
+public class UsuarioBRL
+{
+    public UsuarioBRL()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+
+    public static void insrtUsuario(Usuario obj)
+    {
+        UserDSTableAdapters.UsuariosTableAdapter adapter = new UserDSTableAdapters.UsuariosTableAdapter();
+        adapter.Insert(obj.Nombre, obj.NickName, obj.Correo, obj.Password);
+    }
+
+    public static void updateUsuario(Usuario obj)
+    {
+        UserDSTableAdapters.UsuariosTableAdapter adapter = new UserDSTableAdapters.UsuariosTableAdapter();
+        adapter.Update(obj.Codigo_id, obj.Nombre, obj.NickName, obj.Correo, obj.Password);
+    }
+
+    public static void deleteUsuario(int codigo_id)
+    {
+        UserDSTableAdapters.UsuariosTableAdapter adapter = new UserDSTableAdapters.UsuariosTableAdapter();
+        adapter.Delete(codigo_id);
+    }
+}
---
+++ PokeNUR/App_Code/MODEL/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Usuario
+/// </summary>
+public class Usuario
+{
+    public int Codigo_id { get; set; }
+    public string Nombre { get; set; }
+    public string Correo { get; set; }
+    public string NickName { get; set; }
+    public string Password { get; set; }
+    public Usuario()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+}
