c4d1540083066814b55663552b12e2027cac6364
IsabelBalderrama <izabalderrama.a@gmail.com>
2017-09-25 13:55:18 -0400

    Se corrigio el procedimiento almacenado para el login

---
+++ PokeNUR/Database/V-1-6-1/Update V-1-6-1.sql
@@ -0,0 +1,96 @@
+Use [Master]
+GO 
+
+IF  NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'PokeNUR_DB')
+	RAISERROR('La base de datos ''PokeNUR_DB'' no existe. Cree la base de datos primero',16,127)
+GO
+
+USE [PokeNUR_DB]
+GO
+
+PRINT 'Actualizando a la version 1.6.1'
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[usp_VERSION_GetVersion]') AND type in (N'P', N'PC'))
+BEGIN
+
+	RAISERROR('La base de datos no esta inicializada',16,127)
+	RETURN;
+
+END
+
+
+DECLARE @intVersionMayor int
+DECLARE @intVersionMenor int
+DECLARE @intPatch int
+
+EXECUTE [dbo].[usp_VERSION_GetVersion] 
+   @intVersionMayor OUTPUT
+  ,@intVersionMenor OUTPUT
+  ,@intPatch OUTPUT
+
+IF @intVersionMayor IS NULL OR @intVersionMenor IS NULL
+BEGIN
+	
+	RAISERROR('La base de datos no esta inicializada',16,127)
+	RETURN;
+
+END
+
+
+IF @intVersionMayor IS NULL OR @intVersionMenor IS NULL OR NOT (@intVersionMayor = 1 AND @intVersionMenor = 5)
+BEGIN
+	
+	RAISERROR('La base de datos no esta en la version 1.5. Este script solamente se aplica a la version 1.5',16,127)
+	RETURN;
+
+END
+ELSE
+BEGIN
+	
+	PRINT 'Version OK'
+
+END
+GO
+
+
+/****** Object:  StoredProcedure [dbo].[get_usuarioByNickName]    Script Date: 09/25/2017 11:46:11 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[get_usuarioByNickName] 
+	-- Add the parameters for the stored procedure here
+	@pNick varchar(100),
+	@pPass varchar (50)
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+	
+	
+	SELECT [codigo_id],
+			[nombre],
+			[nickName],
+			[correo],
+			[password]
+	FROM  [dbo].[tblUsuario]
+	WHERE [nickName]=@pNick
+	AND    [password]=@pPass;
+	
+	
+END
+
+
+
+DELETE FROM [dbo].[tbl_Version]
+GO
+
+INSERT INTO [dbo].[tbl_Version]
+           ([versionMayor]
+           ,[versionMenor]
+           ,[patch])
+     VALUES
+           (1
+           ,6
+           ,1)
+GO
