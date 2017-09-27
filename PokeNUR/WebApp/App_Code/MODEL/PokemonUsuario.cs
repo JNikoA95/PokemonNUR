9b4d5936e936c93e150fca41c0b9d8d65691a714
marceloArrazola <marcelo.arrazola88@gmail.com>
2017-09-26 20:23:37 -0400

    poke usuario

---
+++ PokeNUR/WebApp/App_Code/MODEL/PokemonUsuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de PokemonUsuario
+/// </summary>
+public class PokemonUsuario
+{
+    public string nick { get; set; }
+    public string Correo { get; set; }
+    public string nombre { get; set; }
+    public PokemonUsuario()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+}
