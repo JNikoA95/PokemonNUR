344dd5f0addfd0ed3226be030964320ec9be078e
JNikoA95 <andradenik09@gmail.com>
2017-09-26 20:54:26 -0400

    Se creo el script de Initialize para la base de datos

---
+++ PokeNUR/Database/V-1-1-0/Initialize V-1-1-0.sql
@@ -0,0 +1,17 @@
+INSERT INTO [PokeNUR_DB].[dbo].[tblPokemons]
+           ([nombre])
+     VALUES
+           ('Charmander')
+GO
+
+INSERT INTO [PokeNUR_DB].[dbo].[tblPokemons]
+           ([nombre])
+     VALUES
+           ('Bulbasaur')
+GO
+
+INSERT INTO [PokeNUR_DB].[dbo].[tblPokemons]
+           ([nombre])
+     VALUES
+           ('Squirtle')
+GO
