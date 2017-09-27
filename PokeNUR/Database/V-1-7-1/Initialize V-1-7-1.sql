d26747a7ce342807b6eef8e73cdb9ba56449c012
JNikoA95 <andradenik09@gmail.com>
2017-09-26 21:34:27 -0400

    initialize

--- PokeNUR/Database/V-1-7-1/Initialize V-1-7-1.sql
+++ PokeNUR/Database/V-1-7-1/Initialize V-1-7-1.sql
@@ -1,17 +1,33 @@
-INSERT INTO [PokeNUR_DB].[dbo].[tblPokemons]
+USE [PokeNUR_DB]
+GO
+
+INSERT INTO [dbo].[tblPokemons]
            ([nombre])
      VALUES
            ('Charmander')
 GO
 
-INSERT INTO [PokeNUR_DB].[dbo].[tblPokemons]
+INSERT INTO [dbo].[tblPokemons]
            ([nombre])
      VALUES
            ('Bulbasaur')
 GO
 
-INSERT INTO [PokeNUR_DB].[dbo].[tblPokemons]
+INSERT INTO [dbo].[tblPokemons]
            ([nombre])
      VALUES
            ('Squirtle')
+GO
+
+DELETE FROM [dbo].[tbl_Version]
+GO
+
+INSERT INTO [dbo].[tbl_Version]
+           ([versionMayor]
+           ,[versionMenor]
+           ,[patch])
+     VALUES
+           (1
+           ,7
+           ,1)
 GO
