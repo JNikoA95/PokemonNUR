7c5277b8d6629d655d96286ea0e167c71c9b8143
AleSapiain <alejandrasapiain.as@gmail.com>
2017-10-08 21:14:46 -0400

    creacion de servicios rest con laravel

--- PokeNUR/App Movil/PokeNUR/app/src/main/java/pokenur/pokenur/MainActivity.java
+++ PokeNUR/App Movil/PokeNUR/app/src/main/java/pokenur/pokenur/MainActivity.java
@@ -1,13 +1,15 @@
 package pokenur.pokenur;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);S
     }
 }
