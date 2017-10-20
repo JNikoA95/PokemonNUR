3d607a460eb959d8da83e1e55980c550a29327ed
Marcelo <marcelo.arrazola88@gmail.com>
2017-10-20 16:43:01 -0400

    Seeds PokemonAtaque

--- PokeNUR/laravel/RestApi/database/seeds/tblPokemonAtaqueTableSeeder.php
+++ PokeNUR/laravel/RestApi/database/seeds/tblPokemonAtaqueTableSeeder.php
@@ -13,8 +13,54 @@ class tblPokemonAtaqueTableSeeder extends Seeder
     {
         //
 		DB::table('tblpokemonataque')->insert([
-			'pokemon_id'=> '',
-			'ataque_id'=> '',
+			'pokemon_id'=> '1',
+			'ataque_id'=> '1',
+		]);0
+	DB::table('tblpokemonataque')->insert([
+			'pokemon_id'=> '1',
+			'ataque_id'=> '2',
 		]);
+
+	DB::table('tblpokemonataque')->insert([
+			'pokemon_id'=> '1',
+			'ataque_id'=> '41',
+		]);
+		DB::table('tblpokemonataque')->insert([
+			'pokemon_id'=> '1',
+			'ataque_id'=> '39',
+		]);
+		DB::table('tblpokemonataque')->insert([
+			'pokemon_id'=> '2',
+			'ataque_id'=> '6',
+		]);
+		DB::table('tblpokemonataque')->insert([
+			'pokemon_id'=> '2',
+			'ataque_id'=> '7',
+		]);
+		DB::table('tblpokemonataque')->insert([
+			'pokemon_id'=> '2',
+			'ataque_id'=> '38',
+		]);
+		DB::table('tblpokemonataque')->insert([
+			'pokemon_id'=> '2',
+			'ataque_id'=> '41',
+		]);
+		DB::table('tblpokemonataque')->insert([
+			'pokemon_id'=> '3',
+			'ataque_id'=> '9',
+		]);
+		DB::table('tblpokemonataque')->insert([
+			'pokemon_id'=> '3',
+			'ataque_id'=> '12',
+		]);
+		DB::table('tblpokemonataque')->insert([
+			'pokemon_id'=> '3',
+			'ataque_id'=> '11',
+		]);
+		DB::table('tblpokemonataque')->insert([
+			'pokemon_id'=> '3',
+			'ataque_id'=> '41',
+		]);
+
     }
 }
