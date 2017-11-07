f61e902c3cf82da9e5eea466b71190487d437358
JNikoA95 <andradenik09@gmail.com>
2017-11-07 14:27:32 -0400

    version 1.9.0 y correcciones a las versiones anteriores

---
+++ PokeNUR/Database/V-1-10-0/Update V-1-10-0.sql
@@ -0,0 +1,109 @@
+Use [Master]
+GO 
+
+IF  NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'PokeNUR_DB')
+	RAISERROR('La base de datos ''PokeNUR_DB'' no existe. Cree la base de datos primero',16,127)
+GO
+
+USE [PokeNUR_DB]
+GO
+
+PRINT 'Actualizando a la version 1.9.0'
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[usp_VERSION_GetVersion]') AND type in (N'P', N'PC'))
+BEGIN
+
+	RAISERROR('La base de datos no esta inicializada',16,127)
+	RETURN;
+
+END
+
+
+DECLARE @intVersionMayor int
+DECLARE @intVersionMenor int
+DECLARE @intPatch int
+
+EXECUTE [dbo].[usp_VERSION_GetVersion] 
+   @intVersionMayor OUTPUT
+  ,@intVersionMenor OUTPUT
+  ,@intPatch OUTPUT
+
+IF @intVersionMayor IS NULL OR @intVersionMenor IS NULL OR @intPatch IS NULL
+BEGIN
+	
+	RAISERROR('La base de datos no esta inicializada',16,127)
+	RETURN;
+
+END
+
+
+IF @intVersionMayor IS NULL OR @intVersionMenor IS NULL OR NOT (@intVersionMayor = 1 AND @intVersionMenor = 8 AND @intPatch = 1)
+BEGIN
+	
+	RAISERROR('La base de datos no esta en la version 1.8.1. Este script solamente se aplica a la version 1.8.1',16,127)
+	RETURN;
+
+END
+ELSE
+BEGIN
+	
+	PRINT 'Version OK'
+
+END
+GO
+
+USE [PokeNUR_DB]
+GO
+
+/****** Object:  Table [dbo].[tblPokemonUsuario]    Script Date: 11/07/2017 13:49:18 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tblPokemonUsuario](
+	[user_id] [int] NOT NULL,
+	[pokemon_id] [int] NOT NULL,
+	[idPkUser] [int] NOT NULL,
+ CONSTRAINT [PK_tblPokemonUsuario] PRIMARY KEY CLUSTERED 
+(
+	[idPkUser] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Nicolas Andrade
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE get_tblPokemonByUsuarioId
+	-- Add the parameters for the stored procedure here
+	@prmUsuarioId int
+AS
+BEGIN
+	
+	Select P.*
+	FROM tblPokemons P, tblPokemonUsuario PU
+	WHERE P.codigo_id = PU.pokemon_id
+	AND PU.user_id = @prmUsuarioId
+END
+GO
+
+DELETE FROM tbl_Version
+
+INSERT INTO [dbo].[tbl_Version]
+           ([versionMayor]
+           ,[versionMenor]
+           ,[patch])
+     VALUES
+           (1
+           ,9
+           ,0)
+GO
